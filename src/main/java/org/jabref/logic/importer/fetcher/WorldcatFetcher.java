321813d20f05eb6a3b2d973d54e912aac0bc494d
Glenn Olsson <glennholsson@gmail.com>
2020-02-23 16:04:37 +0100

    Check title exists

--- src/main/java/org/jabref/logic/importer/fetcher/WorldcatFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/WorldcatFetcher.java
@@ -1,23 +1,40 @@
 package org.jabref.logic.importer.fetcher;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.jabref.logic.importer.EntryBasedFetcher;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
 
 /**
  * Fetcher for Worldcat.org
+ * Example query: http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=srw.ti+all+%22kth%22&wskey={built-in-api-key}
  */
 public class WorldcatFetcher implements EntryBasedFetcher {
 
-	final public static String NAME = "Worldcat_Fetcher" 
+	final static private String NAME = "Worldcat_Fetcher";
+	final static private String BASE_URL = "http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch";
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return NAME;
 	}
 
-	@Override
+	/**
+	 * Make a search based on the title of the BibEntry.
+	 * 
+	 * {@inheritDoc}
+	*/
+	 @Override
 	public List<BibEntry> performSearch(BibEntry entry) {
-		// TODO Auto-generated method stub
+		Optional<String> title = entry.getLatexFreeField(StandardField.TITLE);
+		if (title.isPresent()) {
+			
+		} else {
+			return new ArrayList<>(0);
+		}
 		return null;
 	}
 }
